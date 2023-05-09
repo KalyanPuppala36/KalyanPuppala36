a50543788495a152c535c052cf60f93f749809e9
Venkata Naga Kalyan Puppala <99470597+KalyanPuppala36@users.noreply.github.com>
2023-05-09 12:29:10 +0530

    Update demo.php

--- Believe It/Web - Dev/demo.php
+++ Believe It/Web - Dev/demo.php
@@ -1,3 +1,18 @@
-<?php
-echo "Hello World"
-?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP Demo</title>
+</head>
+
+<body>
+    <h1>PHP Demo</h1>
+    <?php
+        echo "Hello Vysh!, Home 8<br>";
+        $a=12;
+        $b=13;
+        echo $a+$b;
+    ?>
+</body>
+
+</html>
